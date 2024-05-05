74ea05249831b61cae823a0704f8f3638b5c9f54
Jumana Wanass <jumanawanass@hotmail.com>
2024-05-05 03:21:50 +0300

    Added minor changes

---
+++ Backend/API/server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Setup session
+app.use(
+  session({
+    secret: process.env.SECRET, 
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(cors({
+  optionsSuccessStatus: 200,
+  credentials: true,
+}));
+
+// Set up Multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './images'); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // File naming strategy
+  },
+});
+
+const imageFilter = function (req, file, cb) {
+  // Get the file extension
+  const fileExtension = path.extname(file.originalname).toLowerCase();
+
+  // Check if the file type is an image with a valid extension
+  if (
+    file.mimetype.startsWith('image/') &&
+    ['.jpg', '.jpeg', '.png', '.gif'].includes(fileExtension)
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error('File type or extension not supported!'), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: imageFilter,
+  limits: { fileSize: 1024 * 1024 },
+});
+
+// Routers
+const MuseumRouter = require('./routes/museumrouter');
+const AdminRouter = require('./routes/adminrouter');
+const UserRouter = require('./routes/userrouter');
+const EventRouter = require('./routes/eventrouter');
+const MuseumTicketRouter = require('./routes/museumticketrouter');
+const EventTicketRouter = require('./routes/eventticketrouter');
+
+app.get('/', (req, res) => {
+  res.status(200).json({ hi: 'welcome' });
+});
+
+// Mounting routers
+app.use('/museums', MuseumRouter);
+app.use('/admin', AdminRouter);
+app.use('/users', UserRouter);
+app.use('/events', EventRouter);
+app.use('/museumtickets', MuseumTicketRouter);
+app.use('/eventtickets', EventTicketRouter);
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(500).json({ message: 'Logout failed' });
+    } else {
+      res.clearCookie('connect.sid');
+      res.status(200).json({ message: 'Logout successful' });
+    }
+  });
+});
+
+app.get('/checkSession', (req, res) => {
+  const sessionExists = req.session.auth ? true : false;
+  res.status(200).json({ sessionExists });
+});
+
+app.listen(5000, () => console.log(`Server running on http://localhost:5000/`));
